2407d7e75007e4c533a459f31d0dfbdc2c9c464b
Menardo Madiumba <Menardotera@gmail.com>
2025-07-18 18:30:42 +0200

    Programms valider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Hero } from "./Components/Hero/Hero";
 import { Navbar } from "./Components/Navbar/Navbar";
+import { Programs } from "./Components/Programs/Programs";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Programs />
     </>
   );
 }
