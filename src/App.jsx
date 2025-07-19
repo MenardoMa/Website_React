e22ef185703cc799d99cfe08d5f56cedf0178a45
Menardo Madiumba <Menardotera@gmail.com>
2025-07-19 18:17:07 +0200

    Ajout de props pour le titre

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Hero } from "./Components/Hero/Hero";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { Programs } from "./Components/Programs/Programs";
+import { Title } from "./Components/Title/Title";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Programs />
+      <div className="container">
+        <Title subTitle="OUR PROGRAM" title="What We Offer" />
+        <Programs />
+      </div>
     </>
   );
 }
